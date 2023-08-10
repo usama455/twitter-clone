2a8861f7a218d4399b9a5a5c32e1e658c54cc464
Muhammad Usama <usama@Muhammads-MacBook-Pro.local>
2023-08-10 13:05:25 +0500

    db connection retries added

--- server/src/services/database/index.js
+++ server/src/services/database/index.js
@@ -1,13 +1,30 @@
 import mongoose from "mongoose";
 import { logger } from "../../utils";
 const mongoURI = process.env.DATABASE_URI;
+const maxRetries = 3; // Maximum number of connection retry attempts
 
-export const connectDatabase = () => {
+export const connectDatabase = (retryCount = 0) => {
   mongoose.connect(mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
+
   const db = mongoose.connection;
-  db.on("error", (error) => logger.error(error));
-  db.once("open", () => console.log("DB Connected Successfully!"));
+
+  db.on("error", (error) => {
+    logger.error(error);
+    if (retryCount < maxRetries) {
+      console.log(`Error in connecting to the database. Retrying... (Attempt ${retryCount + 1} of ${maxRetries})`);
+      setTimeout(() => {
+        connectDatabase(retryCount + 1); // Retry the connection
+      }, 2000); // Delay for 2 seconds before retrying
+    } else {
+      console.log(`Max connection retries reached (${maxRetries}). Exiting...`);
+      process.exit(1); // Exit the process after max retries
+    }
+  });
+
+  db.once("open", () => {
+    console.log("DB Connected Successfully!");
+  });
 };
